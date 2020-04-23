76268a1e70cff7b476c191d701e67dc29b2ece45
JackalStew <no@no.no>
2020-04-23 16:39:36 +0100

    Improved SSH port forwarding enable script

--- conf/ssh/enable_tcp_fwd.sh
+++ conf/ssh/enable_tcp_fwd.sh
@@ -1,3 +1,21 @@
-sed -i 's/AllowTcpForwarding no/AllowTcpForwarding yes/g' /etc/ssh/sshd_config
-echo GatewayPorts yes >> /etc/ssh/sshd_config
+#!/bin/bash
+conf_file_set () {
+    fname=$1
+    key=$2
+    val=$3
+    regex="#\?$key.*"
+    conf_line="$key $val"
+
+    if grep --quiet "$regex" $fname
+    then
+        sed -i "s/$regex/$conf_line/g" $fname
+    else
+        echo $conf_line >> $fname
+    fi
+}
+
+ssh_conf_file=/etc/ssh/sshd_config
+conf_file_set $ssh_conf_file AllowTcpForwarding yes
+conf_file_set $ssh_conf_file GatewayPorts yes
+
 echo "Port forwarding enabled. IP address is: $(hostname -I)"
